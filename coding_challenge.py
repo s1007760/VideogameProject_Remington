e1f2037cc7d256e6998f0446d4e1eb68c05b6362
s1007760 <164066436+s1007760@users.noreply.github.com>
2026-02-18 15:45:19 +0000

    Added first guess set up of the functions

--- coding_challenge.py
+++ coding_challenge.py
@@ -2,5 +2,12 @@
 #Similarly, the 9-digit number, 134217728 = 8^9, is a ninth power.
 #How many n digit positive integers exist which are also an nth power?
 
+def doing_powers():
+    #doing math that multiplies n digit positive integers to every power.
+
+def checking_digit_power_similarity():
+    #checking if the number of digits matched the power
+    #if true, add to amount 
+
 print("hello")
 print("goodbye")
